fe3f729f9fc3560171362f94cfa767a0299bad7a
hanifrev <hanifrev@gmail.com>
2021-10-18 19:43:30 +0700

    add category card

--- components/category_card.js
+++ components/category_card.js
@@ -1,9 +1,19 @@
+/* eslint-disable react/jsx-key */
+import Image from 'next/image'
 import data from '../dummy/service_cat.json'
 
 const Category_Card = () => {
     return (
-        <div>
-
+        <div className="flex flex-col md:flex-row">
+            {data.map((item, idx) => (
+                <div key={idx} className="mx-auto pb-8">
+                    <div className="w-full mx-auto text-center pb-6">
+                        <Image src={item.img} alt='image' width={70} height={70} />
+                    </div>
+                    <div className="w-full mx-auto text-center pb-4 text-bold text-xl">{item.title}</div>
+                    <div className="w-9/12 mx-auto text-center text-base">{item.description}</div>
+                </div>
+            ))}
         </div>
     )
 }
---
+++ components/popup_modal.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const Popup_Modal = () => {
+
+    // const [modal, setModal] = useState(false)
+
+    return (
+        
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default Popup_Modal;
