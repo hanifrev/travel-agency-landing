37cc6c0588c3e8ecf3528b276f47e77b5c0bbc11
hanifrev <hanifrev@gmail.com>
2021-10-27 07:39:09 +0700

    add image mock on destination card

--- components/destination_card.js
+++ components/destination_card.js
@@ -6,7 +6,9 @@ const Destination_Card = () => {
         <div className="flex flex-col md:flex-row mx-auto">
             {data.map((item, idx) => (
                 <div key={idx}>
-                    <div>{item.image}</div>
+                    <div>
+                        <Image src={item.image} alt="img" width={630} height={915} />
+                    </div>
                     <div>{item.destination}</div>
                     <div>{item.price}</div>
                     <div>{item.time}</div>
