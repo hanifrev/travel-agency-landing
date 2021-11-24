9c23ca19ca37052c98d08e3664b67c63338ca117
hanifrev <hanifrev@gmail.com>
2021-11-24 14:43:18 +0700

    feat: mobile navigation menu

---
+++ components/nav-mobile/MenuButton.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function MenuButton({click}) {
+  return (
+    <div id="menu-button-div" className="-mr-2 flex items-center md:hidden">
+      <button
+        type="button"
+        onClick={click}
+        className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+        id="main-menu"
+        aria-label="Main menu"
+        aria-haspopup="true"
+      >
+        <svg
+          className="h-6 w-6"
+          stroke="currentColor"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+        
+      </button>
+    </div>
+  );
+}
--- components/sidebar_nav.js
+++ components/sidebar_nav.js
@@ -1,44 +1,40 @@
 import { useState } from "react"
 import Link from 'next/link';
-import data from '../dummy/navbar.json'
+import data from '../dummy/navbar_mobile.json'
 
 const SideNav = () => {
 
-    const [show, setShow] = useState()
-    const [visible, setVisible] = useState(false)
+    const [open, setOpen] = useState()
+    const [visible, setVisible] = useState(true)
 
     return (
         <div>
             {data.map((item, key) => {
                 return (
-                    <div key={key} className="" onClick={() => {
-                        setShow(item.key)
+                    <div key={key} className="pane" onClick={()=> {
+                        setOpen(item.key);
                         setVisible(false)
                     }} >
-                        Toggle
+                        <div className="pane-item">=</div>
                         {visible && key === "null" ? (
                             <>
-                                <div>
-                                    <li className="list-none mx-9 font-semibold" key={idx}>
-                                        <Link href={item.link}>
-                                            {item.title}
-                                        </Link>
-                                    </li>
-                                </div>
+                                <Link href={item.link} className="pane-item">{item.title}</Link>
+                                <Link href={item.link2} className="pane-item">{item.title2}</Link>
+                                <Link href={item.link3} className="pane-item">{item.title3}</Link>
+                                <Link href={item.link4} className="pane-item">{item.title4}</Link>
+                                <Link href={item.link5} className="pane-item">{item.title5}</Link>
                             </>
-                        ) : null }
-                        {show === key ? (
+                        ) : null}
+                        {open === key ? (
                             <>
-                                <div>
-                                    <li className="list-none mx-9 font-semibold" key={idx}>
-                                        <Link href={item.link}>
-                                            {item.title}
-                                        </Link>
-                                    </li>
-                                </div>
+                                <Link href={item.link} className="pane-item">{item.title}</Link>
+                                 <Link href={item.link2} className="pane-item">{item.title2}</Link>
+                                <Link href={item.link3} className="pane-item">{item.title3}</Link>
+                                <Link href={item.link4} className="pane-item">{item.title4}</Link>
+                                <Link href={item.link5} className="pane-item">{item.title5}</Link>
                             </>
                         ) : null }
-    
+
                     </div>
                 )
             })}
